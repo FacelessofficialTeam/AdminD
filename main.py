7cf32d44cc6e8573160f61ed35fc4f261ae6d176
FacelessofficialTeam <facelessofficial9@gmail.com>
2024-10-21 22:00:56 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,104 @@
+# List of admin panel paths to be used in your tool
+admin_panels = [
+    "/acceso.asp", "/acceso.php", "/access/", "/access.php", "/account/",
+    "/account.asp", "/account.html", "/account.php", "/acct_login/", "/_adm_/",
+    "/_adm/", "/adm/", "/adm2/", "/2083", "/adm/admloginuser.asp",
+    "/adm/admloginuser.php", "/adm.asp", "/adm_auth.asp", "/adm_auth.php",
+    "/adm.html", "/_admin_/", "/admin/", "/Admin/", "/ADMIN/", "/admin1/",
+    "/admin1.asp", "/admin1.html", "/admin1.php", "/admin2/", "/admin2.asp",
+    "/admin2.html", "/admin2/index/", "/admin2/index.asp", "/admin2/index.php",
+    "/admin2/login.asp", "/admin2/login.php", "/admin2.php", "/admin3/",
+    "/admin4/", "/admin4_account/", "/admin4_colon/", "/admin5/",
+    "/admin/account.asp", "/admin/account.html", "/admin/account.php",
+    "/admin/add_banner.php/", "/admin/addblog.php", "/admin/add_gallery_image.php",
+    "/admin/add.php", "/admin/add-room.php", "/admin/add-slider.php",
+    "/admin/add_testimonials.php", "/admin/admin/", "/admin/adminarea.php",
+    "/admin/admin.asp", "/admin/AdminDashboard.php", "/admin/admin-home.php",
+    "/admin/AdminHome.php", "/admin/admin.html", "/admin/admin_index.php",
+    "/admin/admin_login.asp", "/admin/admin-login.asp", "/admin/adminLogin.asp",
+    "/admin/admin_login.html", "/admin/admin-login.html", "/admin/adminLogin.html",
+    "/admin/admin_login.php", "/admin/admin-login.php", "/admin/adminLogin.php",
+    "/admin/admin_management.php", "/admin/admin.php", "/admin/admin_users.php",
+    "/admin/adminview.php", "/admin/adm.php", "/admin_area/", "/adminarea/",
+    "/admin_area/admin.asp", "/adminarea/admin.asp", "/admin_area/admin.html",
+    "/adminarea/admin.html", "/admin_area/admin.php", "/adminarea/admin.php",
+    "/admin_area/index.asp", "/adminarea/index.asp", "/admin_area/index.html",
+    "/adminarea/index.html", "/admin_area/index.php", "/adminarea/index.php",
+    "/admin_area/login.asp", "/adminarea/login.asp", "/admin_area/login.html",
+    "/adminarea/login.html", "/admin_area/login.php", "/adminarea/login.php",
+    "/admin.asp", "/admin/banner.php", "/admin/banners_report.php",
+    "/admin/category.php", "/admin/change_gallery.php", "/admin/checklogin.php",
+    "/admin/configration.php", "/admincontrol.asp", "/admincontrol.html",
+    "/admincontrol/login.asp", "/admincontrol/login.html", "/admincontrol/login.php",
+    "/admin/control_pages/admin_home.php", "/admin/controlpanel.asp",
+    "/admin/controlpanel.html", "/admin/controlpanel.php", "/cpanel/",
+    "/cPanel/", "/cpanel_file/", "/cp.asp", "/cp.html", "/cp.php",
+    "/customer_login/", "/database_administration/", "/Database_Administration/",
+    "/db/admin.php", "/directadmin/", "/dir-login/", "/editor/", "/edit.php",
+    "/evmsadmin/", "/ezsqliteadmin/", "/fileadmin/", "/fileadmin.asp", "/fileadmin.html",
+    "/fileadmin.php", "/formslogin/", "/forum/admin", "/globes_admin/", "/home.asp",
+    "/home.html", "/home.php", "/hpwebjetadmin/", "/include/admin.php",
+    "/includes/login.php", "/Indy_admin/", "/instadmin/", "/interactive/admin.php",
+    "/irc-macadmin/", "/links/login.php", "/LiveUser_Admin/", "/login/", "/login1/",
+    "/login.asp", "/login_db/", "/loginflat/", "/login.html", "/login/login.php",
+    "/login.php", "/login-redirect/", "/logins/", "/login-us/", "/logon/",
+    "/logo_sysadmin/", "/Lotus_Domino_Admin/", "/macadmin/", "/mag/admin/",
+    "/maintenance/", "/manage_admin.php", "/manager/", "/manager/ispmgr/",
+    "/manuallogin/", "/memberadmin/", "/memberadmin.asp", "/memberadmin.php",
+    "/members/", "/memlogin/", "/meta_login/", "/modelsearch/admin.asp",
+    "/modelsearch/admin.html", "/modelsearch/admin.php", "/modelsearch/index.asp",
+    "/modelsearch/index.html", "/modelsearch/index.php", "/modelsearch/login.asp",
+    "/modelsearch/login.html", "/modelsearch/login.php", "/moderator/",
+    "/moderator/admin.asp", "/moderator/admin.html", "/moderator/admin.php",
+    "/moderator.asp", "/moderator.html", "/moderator/login.asp", "/moderator/login.html",
+    "/moderator/login.php", "/moderator/login.php/", "/monitor_admin/",
+    "/news_admin/", "/new_admin/", "/newadmin/", "/newlogin/", "/news/admin/",
+    "/newsletters/admin/", "/online/admin/", "/onlineAdmin/", "/op_auth/admin.html",
+    "/op_auth/admin.php", "/orders_admin/", "/page_admin.php", "/panel/admin.php",
+    "/part_admin/", "/passwords/", "/passwords.asp", "/passwords.php",
+    "/password-admin/", "/pm_admin/", "/pointlogin/", "/port/admin/",
+    "/privateadmin/", "/production/", "/production/index.php", "/project_manager/",
+    "/protection.php", "/qt2admin/", "/quickpay/admin.php", "/radmin/",
+    "/realtime_login/", "/reports/admin.php", "/reports/login.php",
+    "/restrict/admin.php", "/robotlogin/", "/root/admin.php", "/root/admin.html",
+    "/root/admin.php", "/root/login.php", "/root_login/", "/sadmin/",
+    "/s_admin/", "/scripts/admin/", "/session_login/", "/session_login.asp",
+    "/session_login.html", "/session_login.php", "/session/login.php",
+    "/setup/admin/", "/setup.php", "/setup_page/admin.php", "/shadmin/",
+    "/shop-admin/", "/shop_login/", "/siteadmin/", "/site_admin/",
+    "/site-admin/", "/site1/admin/", "/site1/login/", "/site2/admin/",
+    "/site2/login/", "/sitemap_admin/", "/sitemaps/admin.php", "/src/admin/",
+    "/staff_login/", "/statistics/admin/", "/statistics/login.php",
+    "/super_admin/", "/superadmin/", "/support_admin/", "/system/login/",
+    "/system/login.asp", "/system_login/", "/system/log.php",
+    "/sysadmin/", "/tech-login/", "/tech_login/", "/test/admin/",
+    "/testlogin/", "/testLogin/", "/thadmin/", "/tinylogin/",
+    "/tracking/admin.php", "/tracking/login.php", "/tracking_login/",
+    "/useradmin/", "/user/admin.php", "/userLogin.php", "/webadmin/",
+    "/web_admin/", "/web-admin/", "/webcontrol/", "/webcontrol.asp",
+    "/webmaster/admin.asp", "/webmaster/login.php", "/webmaster/login.asp",
+    "/wso/", "/wsys/admin.php", "/wsys/login.php", "/xadmin/",
+    "/xaccess/", "/xdmin/", "/your_admin/", "/youraccount/"
+]
+
+# Function to check if an admin panel exists
+def check_admin_panel(url):
+    for panel in admin_panels:
+        full_url = f"{url}{panel}"
+        # Here you can add code to make a request to the full_url and check the response
+        print(f"Checking: {full_url}")
+
+# Main function to prompt the user for a website link
+def main():
+    target_url = input("Website er link din (e.g., http://example.com): ")
+    # Validate URL format if necessary
+    if not target_url.startswith("http://") and not target_url.startswith("https://"):
+        print("Valid URL den, http:// ba https:// diye shuru hote hobe.")
+        return
+    
+    check_admin_panel(target_url)
+
+# Run the main function
+if __name__ == "__main__":
+    main()
+    
